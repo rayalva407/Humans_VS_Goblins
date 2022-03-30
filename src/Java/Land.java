f6fdbdc6a4d68a1c9e8fdc7ccca16fa786192ee0
rayalva407 <rayalva407@gmail.com>
2022-03-30 14:32:34 -0600

    Can output a string representing each object

--- src/Java/Land.java
+++ src/Java/Land.java
@@ -1,10 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Land {
 
     char[][] gameGrid;
     ArrayList<Goblin> goblins = new ArrayList<>();
     Human human = new Human();
+
+    Land(int length, int height) {
+        gameGrid = new char[height][length];
+
+        for (char[] row : gameGrid) {
+            Arrays.fill(row, '*');
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder landStr = new StringBuilder();
+        for (char[] chars : gameGrid) {
+            for (char aChar : chars) {
+                landStr.append(aChar);
+            }
+            landStr.append("\n");
+        }
+        return landStr.toString();
+    }
 
 
 }
--- src/Java/Main.java
+++ src/Java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Land land = new Land(5, 5);
+        Human human = new Human();
+        Goblin goblin = new Goblin();
+        System.out.println(land);
+        System.out.println(human);
+        System.out.println(goblin);
     }
 }
